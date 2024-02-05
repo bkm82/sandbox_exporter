f643587c137aa8c2af35f56eb474a65b3505127a
Bray Moll <bkm82@nau.edu>
2024-02-04 21:28:32 -0700

    Add heads count, working api

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import random
 
 from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
+import prometheus_client
 import uvicorn
 
 app = FastAPI()
@@ -18,6 +19,21 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+heads_count = prometheus_client.Counter(
+    "heads_count",
+    "Number of heads",
+)
+
+tails_count = prometheus_client.Counter(
+    "tails_count",
+    "Number of tails",
+)
+
+flip_count = prometheus_client.Counter(
+    "flip_count",
+    "Number of flips",
 )
 
 @app.get("/flip-coins")
@@ -38,10 +54,21 @@ async def flip_coins(times=None):
 
     tails = times_as_int - heads
 
+    heads_count.inc(heads)
+    tails_count.inc(tails)
+    flip_count.inc(times_as_int)
+
     return {
         "heads": heads,
         "tails": tails,
     }
+
+@app.get('/metrics')
+def get_metrics():
+    return Response(
+        media_type="text/plain",
+        content = prometheus_client.generate_latest(),
+    )
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
